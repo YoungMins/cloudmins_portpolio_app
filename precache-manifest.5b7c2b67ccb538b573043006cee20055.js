c80ff35d55f08e2a5336dc992910ede6daf7ee14
김영민 <nanyoungmin@gmail.com>
2020-07-13 15:29:30 +0900

    Updates

---
+++ precache-manifest.5b7c2b67ccb538b573043006cee20055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529e584a32d2e73c42e372e68c2bd6dc",
+    "url": "/cloudmins_portpolio_app/index.html"
+  },
+  {
+    "revision": "f26ba17bf31c46567567",
+    "url": "/cloudmins_portpolio_app/static/css/main.dd1546f6.chunk.css"
+  },
+  {
+    "revision": "5d56026a4edf115ed590",
+    "url": "/cloudmins_portpolio_app/static/js/2.b57e0a01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cloudmins_portpolio_app/static/js/2.b57e0a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26ba17bf31c46567567",
+    "url": "/cloudmins_portpolio_app/static/js/main.85268251.chunk.js"
+  },
+  {
+    "revision": "53d724eb65eea2b0b494",
+    "url": "/cloudmins_portpolio_app/static/js/runtime-main.3aa51a0c.js"
+  }
+]);
